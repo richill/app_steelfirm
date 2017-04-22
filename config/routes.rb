33170eeb128041f85b4ee8914771d902a5129d2c
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-04-22 17:41:18 +0100

    updated route for homepg

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/homepg'
-
-  root 'static_pages#home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#homepg'
 end
